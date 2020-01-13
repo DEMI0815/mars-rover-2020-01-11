e3d0564e280fb6a998ada4a4fdafaf4ca8377b40
yangchao <chao.yang@thoughtworks.com>
2020-01-14 00:44:30 +0800

    given more than 1 commends

--- src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/MarsRoverTest.java
@@ -2,17 +2,24 @@ package com.thoughtworks.marsrover;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 public class MarsRoverTest {
 
+    private MarsRover marsRover = new MarsRover(0, 0, Direction.N);
+
     @Test
     public void given_one_commend() {
-        MarsRover marsRover = new MarsRover(0, 0, Direction.N);
         marsRover.run("M");
         MarsRover result = new MarsRover(0, 1, Direction.N);
+
+        assertEquals(marsRover.toString(), result.toString());
+    }
+
+    @Test
+    public void given_more_than_one_commends() {
+        marsRover.run("RMLMRM");
+        MarsRover result = new MarsRover(2, 1, Direction.E);
 
         assertEquals(marsRover.toString(), result.toString());
     }
